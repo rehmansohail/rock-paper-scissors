454d0658f49bf8504ab667ddd25ab7f3f78092cd
Sohail Rehman <sohailmalikraja@gmail.com>
2023-05-21 00:01:00 +0530

    added logic via js

--- logic.js
+++ logic.js
@@ -1,5 +1,62 @@
 let arr=['rock','paper','scissors'];
 
+let userScore=0,compScore=0;
+var gameResult = "";
+var resultText = document.getElementById("result-text");
+
+var gameButton = document.getElementsByClassName("btn");
+
+function handleButtonClick(button) {
+    var buttonText = button.innerText;
+    buttonText=buttonText.toLowerCase();
+    let userInput = buttonText;
+    let compInput = random(arr);
+    // Call your function and pass the buttonText as input
+    let result = game(userInput,compInput);
+
+    if (userScore === 5 || compScore===5) {
+        // Display a message
+        alert("Game over! Please reload the page for a new game.");
+        gameButton.disabled = true; // Disable the button
+        userScore=0;
+        compScore=0;
+      }
+
+    if(result==1){
+                userScore++;
+                console.log(`you won ${userInput} beats ${compInput}`)
+                console.log(`score: ${userScore}-${compScore}`)
+            }
+            else if(result==-1){
+                compScore++;
+                console.log(`you lost ${compInput} beats ${userInput}`)
+                console.log(`score: ${userScore}-${compScore}`)
+            }
+            else{
+                console.log(`match drawn`)
+                console.log(`score: ${userScore}-${compScore}`)
+            }
+            gameResult = `${userScore}-${compScore}`;
+            let res1=`you won ${userInput} beats ${compInput}`;
+            let res2=`you lost ${compInput} beats ${userInput}`;
+            if(result==1){
+                resultText.textContent= res1+" : "+gameResult;
+            }
+            else if(result==0){
+                resultText.textContent="Match Drawn : "+gameResult;
+            }
+            else{
+            resultText.textContent = res2+" : " + gameResult;
+        }
+
+  }
+  
+//   function yourFunction(buttonText) {
+//     // Do something with the buttonText
+//     console.log("Button text:", buttonText);
+//   }
+  
+
 
 
 
@@ -14,13 +71,10 @@ function game(userInput,compInput){
         else if(compInput=='paper')return 0
         else return -1
     }
-    else if(userInput=='scissors'){
+    else{
         if(compInput=='rock')return -1
         else if(compInput=='paper')return 1
         else return 0;
-    }
-    else{
-        return 2
     }
 }
 
@@ -31,33 +85,36 @@ function random(array){
     }
     return arr[i]
 }
-let userScore=0,compScore=0;
-while(1){
-    if(userScore==5 || compScore==5){
-        console.log('Thanks for playing')
-        break
-    }
-    let userInput = prompt("Enter the input (rock,paper,scissors):")
-    userInput=userInput.toLowerCase();
-    let compInput = random(arr);
-    let result = game(userInput,compInput)
-    if(result==1){
-        userScore++;
-        console.log(`you won ${userInput} beats ${compInput}`)
-        console.log(`score: ${userScore}-${compScore}`)
-    }
-    else if(result==-1){
-        compScore++;
-        console.log(`you lost ${compInput} beats ${userInput}`)
-        console.log(`score: ${userScore}-${compScore}`)
-    }
-    else if(result==0) {
-        console.log(`match drawn`)
-        console.log(`score: ${userScore}-${compScore}`)
-    }
-    else{
-        console.log('Invalid input, Enter again')
-    }
 
-}
+
+
+
+// while(1){
+//     if(userScore==5 || compScore==5){
+//         console.log('Thanks for playing')
+//         break
+//     }
+//     let userInput = prompt("Enter the input (rock,paper,scissors):")
+//     userInput=userInput.toLowerCase();
+//     let compInput = random(arr);
+//     let result = game(userInput,compInput)
+//     if(result==1){
+//         userScore++;
+//         console.log(`you won ${userInput} beats ${compInput}`)
+//         console.log(`score: ${userScore}-${compScore}`)
+//     }
+//     else if(result==-1){
+//         compScore++;
+//         console.log(`you lost ${compInput} beats ${userInput}`)
+//         console.log(`score: ${userScore}-${compScore}`)
+//     }
+//     else if(result==0) {
+//         console.log(`match drawn`)
+//         console.log(`score: ${userScore}-${compScore}`)
+//     }
+//     else{
+//         console.log('Invalid input, Enter again')
+//     }
+
+// }
 
